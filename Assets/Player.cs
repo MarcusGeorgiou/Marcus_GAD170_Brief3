940ccb145f308cee944ecec59239bea2036ec0ba
MarcusGeorgiou <1027503@student.sae.edu.au>
2022-04-16 12:10:35 +1000

    Nothing
    Literally only added sprites and a limit to the player movement because i forgot how to set up for it

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private float lowerLimit = -14f;
+    private float upperLimit = 14f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
